388b0dca004db2ad7606fde3a8c78a05df5b8a5c
Joshua Asiedu <asiedujosh@yahoo.com>
2022-07-11 10:19:27 +0000

    Home export Api v4

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,9 @@
-const Home = (req, res) => {
-  res.status(201).send("Welcome to Rhemi Api");
+const Home = async (req, res) => {
+  try {
+    res.status(201).send("Welcome to Rhemi Api");
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 
 module.exports = Home;
